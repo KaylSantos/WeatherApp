6be9768b54f641503e66632ade63899a1bbdb116
KaylSantos <115588601+KaylSantos@users.noreply.github.com>
2022-12-03 23:46:38 +0100

    Added background image/colour, and retouched some css

--- src/app.js
+++ src/app.js
@@ -69,7 +69,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "e947cb2640f1db92e6a19005bc43b435";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
